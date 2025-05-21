c7738d8006b76e39567beec5553903ff6f7272e0
carlos22-R <rc19102@ues.edu.sv>
2025-05-20 20:53:59 -0600

    prueba v1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import AlgoritmoOrdenamiento.Quicksort;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int[] desordenado = {34, 7, 23, 32, 5, 62};
+
+        int[] ordenado = Quicksort.quickSort(desordenado);
+
+        System.out.println("Original: " + Arrays.toString(desordenado));
+        System.out.println("Ordenado: " + Arrays.toString(ordenado));
     }
+
 }
